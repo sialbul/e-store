fda927bf062031523488fd71e6b3856a5d2020cf
Sibel <sbaslamisli@yahoo.com>
2020-07-01 23:56:32 -0400

    setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Hello from Navbar</h3>
+      </div>
+    );
+  }
+}

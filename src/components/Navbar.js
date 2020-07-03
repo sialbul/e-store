2e9b0254d6cd2b048d4f9e3df061baef8e0c01d3
Sibel <sbaslamisli@yahoo.com>
2020-07-03 00:45:44 -0400

    styling navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.svg";
 import styled from "styled-components";
+import { ButtonContainer } from "./Button";
 
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         {/* https://www.iconfinder.com/icons/1243689/call_phone_icon
 Creative Commons (Attribution 3.0 Unported);
 https://www.iconfinder.com/Makoto_msk  */}
@@ -28,29 +29,16 @@ https://www.iconfinder.com/Makoto_msk  */}
             My Cart
           </ButtonContainer>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
 
-const ButtonContainer = styled.button`
-  text-transform: capitalize;
-  font-size:1.4rem;
-  background:transparent;
-  border:0.05rem solid var(--lightBlue);
-  color: var(--lightBlue);
-  border-radius: 0.5rem;
-  padding: 0.2rem 0.5rem;
-  cursor:pointer;
-  margin:0.2rem 0.5rem 0.5rem 0;
-  transition:all 0.5s ease-in-out;
-  &:hover {
-    background:var(--lightBlue);
-    color:var(--mainBlue);
+const NavWrapper = styled.nav `
+  background: var(--mainBlue);
+  .nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform:capitalize;
   }
-&:focus{
-  outline:none;
-}
-
-
 `;

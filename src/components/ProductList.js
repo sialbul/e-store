c9bed2f13b414ddf0b95fc663526e9b87c80bd66
Sibel <sbaslamisli@yahoo.com>
2020-07-04 00:30:03 -0400

    fetching the products

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,19 +1,31 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
+import { storeProducts } from "../data";
+import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
   state = {
-    products: [],
+    products: storeProducts,
   };
 
   render() {
+    console.log(this.state.products);
     return (
       <React.Fragment>
         <div className="py-5">
           <div className="container">
             <Title name="our" title="products" />
-            <div className="row" />
+
+            <div className="row">
+              <ProductConsumer>
+                {value=>{
+                  return <h1>{value}</h1>
+
+                }}
+              </ProductConsumer>
+            </div>
+
           </div>
         </div>
       </React.Fragment>

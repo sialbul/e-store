3b798d63d1e35a80a4da491fee2517c3c8918780
Sibel <sbaslamisli@yahoo.com>
2020-07-26 23:36:52 -0400

    fetch products

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Hello from Product</h3>
-            </div>
-        )
-    }
+  render() {
+    const { id, title, img, price, inCart } = this.props.product;
+    return (
+      <ProductWrapper className="col-0 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={console.log("you clicked me on the image container")}
+          >
+              <Link to="/details">
+                  <img src={img} alt="product" className="card-img-top"/>
+              </Link>
+          </div>
+        </div>
+      </ProductWrapper>
+    );
+  }
 }
-
+const ProductWrapper = styled.div``;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
-import { storeProducts } from "../data";
 import { ProductConsumer } from "../context";
 
 export default class ProductList extends Component {
-  state = {
-    products: storeProducts
-  };
+
 
   render() {
     return (

0385543870a51b25872fc736903280a1108d553b
Sibel <sbaslamisli@yahoo.com>
2020-08-14 13:02:46 -0400

    protect data in proptypes

--- src/context.js
+++ src/context.js
@@ -8,33 +8,45 @@ const ProductContext = React.createContext();
 //Consumer
 
 class ProductProvider extends Component {
-    state={
-        products: storeProducts, 
-        detailProduct:detailProduct
-    }
+  state = {
+    products: [],
+    detailProduct: detailProduct,
+  };
+  componentDidMount(){
+      this.setProducts();
+  }
+  setProducts = () =>{
+    let tempProducts = [];
+    storeProducts.forEach(item =>{
+      const singleItem = {...item};
+      tempProducts = [...tempProducts,singleItem];
+    });
+    this.setState(()=>{
+        return{products:tempProducts}
+    })
+  };
+  handleDetail = () => {
+    console.log("hello from detail");
+  };
+  addToCart = () => {
+    console.log("hello from add to cart");
+  };
 
-    handleDetail = () =>{
-        console.log("hello from detail");
-
-    }
-
-    addToCart = () =>{
-        console.log("hello from add to cart");
-
-    }
   render() {
-    return <ProductContext.Provider value={{
-        ...this.state, 
-        handleDetail:this.handleDetail,
-        addToCart:this.addToCart,
-    }}
-    
-    >
-        {this.props.children}
-    </ProductContext.Provider>;
+    return (
+      <ProductContext.Provider
+        value={{
+          ...this.state,
+          handleDetail: this.handleDetail,
+          addToCart: this.addToCart,
+        }}
+      >
+        {this.props.children}{" "}
+      </ProductContext.Provider>
+    );
   }
 }
 
 const ProductConsumer = ProductContext.Consumer;
 
-export {ProductProvider, ProductConsumer};
+export { ProductProvider, ProductConsumer };

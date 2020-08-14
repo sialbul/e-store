06029fef797090f0a5babed79805c75548be2994
Sibel <sbaslamisli@yahoo.com>
2020-08-14 12:21:43 -0400

    product styling

--- src/components/Product.js
+++ src/components/Product.js
@@ -68,4 +68,32 @@ const ProductWrapper = styled.div`
     position: relative;
     overflow: hidden;
   }
+  .card-img-top {
+    transition: all 1s linear;
+  }
+  .img-container:hover .card-img-top {
+    transform: scale(1.2);
+  }
+  .cart-btn {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    padding: 0.2rem 0.4rem;
+    background:var(--lightBlue);
+    border:none;
+    color:var(--mainWhite);
+    font-size:1.4rem;
+    border-radius:0.5rem 0 0 0;
+    transform:translate(100%, 100%);
+    transition: all 1s linear;
+
+  }
+  .img-container:hover .cart-btn{
+    transform:translate(0,0);
+  }
+  .cart-btn:hover{
+    color:var(--mainBlue);
+    cursor:pointer;
+
+  }
 `;

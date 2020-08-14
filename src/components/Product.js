183d920f6b5d6b992d8dfaebbe6611b272aaeb85
Sibel <sbaslamisli@yahoo.com>
2020-08-14 11:58:38 -0400

    product styling

--- src/components/Product.js
+++ src/components/Product.js
@@ -35,15 +35,37 @@ export default class Product extends Component {
           </div>
           {/*card footer */}
           <div className="card-footer d-flex justify-content-between">
-            <p className="align-self-center mb-0">
-              {title}
-            </p>
-            <h5 className="text-blue font-italic mb-0"><span className="mr-1">
-              </span></h5>
+            <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span> {price}
+            </h5>
           </div>
         </div>
       </ProductWrapper>
     );
   }
 }
-const ProductWrapper = styled.div``;
+const ProductWrapper = styled.div`
+  .card {
+    border-color: transparent;
+    transition: all 1s linear;
+  }
+  .card-footer {
+    background: transparent;
+    border-top: transparent;
+    transition: all 1s linear;
+  }
+  &:hover {
+    .card {
+      border: 0.04rem solid rgba(0, 0, 0, 0.2);
+      box-shadow: 2px 2px 5px 0px rgba(0, 0, 0, 0.2);
+    }
+    .card-footer {
+      background: rgba(247, 247, 247);
+    }
+  }
+  .img-container {
+    position: relative;
+    overflow: hidden;
+  }
+`;

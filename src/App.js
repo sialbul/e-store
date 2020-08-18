6925a39d0bc67060d051e5ea050272b73096ffa9
Sibel <sbaslamisli@yahoo.com>
2020-08-17 21:48:41 -0400

    setup modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
@@ -8,19 +7,18 @@ import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
+import Modal from "./components/Modal";
 
 function App() {
   return (
     <React.Fragment>
       <Navbar />
       <Switch>
-        <Route exact path="/" component={ProductList} />
-        <Route path="/details" component={Details} />
-        <Route path="/cart" component={Cart} />
-        <Route component={Default} />
+        <Route exact path="/" component={ProductList} />{" "}
+        <Route path="/details" component={Details} />{" "}
+        <Route path="/cart" component={Cart} /> <Route component={Default} />{" "}
       </Switch>
-
-
+      <Modal />
     </React.Fragment>
   );
 }
---
+++ src/components/Modal.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import styled from 'styled-components';
+import {ProductConsumer} from '../context';
+import {ButtonContainer} from './Button';
+import {Link} from 'react-router-dom';
+
+
+
+
+export default class Modal extends Component {
+  render() {
+    return (
+      <div>
+        <h1> hello from modal </h1>{" "}
+      </div>
+    );
+  }
+}

a7a45a90cc4f1139cd079f4d97fe721f3adb8bc5
Sibel <sbaslamisli@yahoo.com>
2020-08-17 22:42:43 -0400

    setup modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -15,15 +15,33 @@ export default class Modal extends Component {
           if (!modalOpen) {
             return null;
           } else {
-            return (<ModalContainer>
-              <div className="container">
-                <div className="row">
-                  <div id="modal" className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize">
-                    <h5>item added to the cart</h5>
+            return (
+              <ModalContainer>
+                <div className="container">
+                  <div className="row">
+                    <div
+                      id="modal"
+                      className="col-8 mx-auto col-md-6 col-lg-4 text-center text-capitalize p-5"
+                    >
+                      <h5>item added to the cart</h5>
+                      <img src={img} className="img-fluid" alt="product" />
+                      <h5>{title}</h5>
+                      <h5 className="text-muted">price : ${price}</h5>
+                      <Link to="/">
+                        <ButtonContainer onClick={() => closeModal()}>
+                          store{" "}
+                        </ButtonContainer>
+                      </Link>
+                      <Link to="/cart">
+                        <ButtonContainer cart onClick={() => closeModal()}>
+                          go to cart
+                        </ButtonContainer>
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </ModalContainer>);
+              </ModalContainer>
+            );
           }
         }}
       </ProductConsumer>
@@ -32,14 +50,16 @@ export default class Modal extends Component {
 }
 
 const ModalContainer = styled.div`
-
-position:fixed;
-top:0;
-left:0;
-right:0;
-bottom:;
-background:rgba(0,0,0,0.3);
-
-
-
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.3);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  #modal {
+    background: var(--mainWhite);
+  }
 `;

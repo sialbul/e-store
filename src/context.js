c2c7fb2d5d687b7a34c3b4c28a6a004a9c1ed634
Sibel <sbaslamisli@yahoo.com>
2020-09-11 21:35:17 -0400

    cart columns-cart total

--- src/context.js
+++ src/context.js
@@ -11,7 +11,7 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: [],
+    cart: storeProducts,
     modalOpen: false,
     modalProduct: detailProduct,
     cartSubTotal: 0,

36c7fbb6b3985eca2f91d27232a0c56ca9ff6ee1
Sibel <sbaslamisli@yahoo.com>
2021-04-05 13:57:19 -0400

    changed the font

--- src/index.js
+++ src/index.js
@@ -8,15 +8,12 @@ import { ProductProvider } from "./context";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <ProductProvider>
-    <Router>
-      <App />
-    </Router>
-  </ProductProvider>,
-  document.getElementById("root")
+    <ProductProvider>
+        <Router>
+            <App />
+        </Router>{" "}
+    </ProductProvider>,
+    document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
